4ac6b7aac5ac6da3a44ab0afe926f87b0ccc225a
Devin Benson <devinbenson@gmail.com>
2020-06-22 21:35:37 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,16 +1,13 @@
 require "pry"
 
-test = [1,2,3,4]
 
 def square_array(array)
   new_array = []
   index = 0
 
-while n<array.length do 
+while index<array.length do 
     x=(array[index]) ** 2 
     new_array.push(x)
-index += 1 
+    index += 1 
+  end
 end
-end
-
-square_array(test)
